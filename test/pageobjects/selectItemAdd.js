7f415d9ac1198cfcd8b8f7860f72a5d3cdf3b4bb
nyleLarsen <nylarseen@gmail.com>
2024-04-26 14:11:49 -0600

    multipleTestsSaucy

--- test/pageobjects/selectItemAdd.js
+++ test/pageobjects/selectItemAdd.js
@@ -25,31 +25,77 @@ class ItemAdd extends OpenPage{
     }
 
 
-
+// NOTE! when running both these tests in one session,
+// the runLength = false needs to run first!
     async AddAllToBag(runLength) {
+        
+        let removeFromCartButtons = await this.removeFromCarty();
         let addToCartButtons = (await this.addToCarty());
-        const initAddBtn = (await this.addToCarty()).length;
-
+        await console.log(removeFromCartButtons.length + " remove why won't this work");
+        if(removeFromCartButtons.length > 0) {
+            await removeFromCartButtons[0].click();
+            await console.log("removed 1 from cart?");
+            //await browser.pause(500);
+        }
+        const initAddBtn = (await this.addToCarty()).length; //this is used for an alt test
+        await console.log(addToCartButtons.length + " add why won't this work");
         await (addToCartButtons[0]).waitForExist({ timeout: 1000 });
         await console.log(addToCartButtons[0] + " cheese is NOT fire");
 
         if (runLength == true) {
+            addToCartButtons = (await this.addToCarty());
+            await console.log('running short test');
             for(let i = 0; i < addToCartButtons.length; i++) {
                 await addToCartButtons[i].click();
             }
             await this.viewCart.click();
         
-            let removeFromCartButtons = await this.removeFromCarty();
+            removeFromCartButtons = await this.removeFromCarty();
 
             if(removeFromCartButtons.length == addToCartButtons.length) {
                 await this.verifyAfterGood.waitForExist({ timeout: 250 });
             }
             else {
+                await console.log("does" + removeFromCartButtons.length + " = " + addToCartButtons.length + "?");
                 await this.verifyAfterBad.waitForExist({ timeout: 250 });
             }
         }
         else if (runLength == false) {
-            await console.log('running long test');
+            await console.log('running longer test');
+            if(addToCartButtons.length > 0) {
+                for(let i = 0; i < 20; i++) {
+                    let removeFromCartButtons = (await this.removeFromCarty());
+                    await console.log("there are " + removeFromCartButtons.length + " remove btns")
+                    if(removeFromCartButtons.length > 0) {
+                            await removeFromCartButtons[0].click();
+                            await console.log("removed 1 from cart?");
+                            //await browser.pause(500);
+                    }
+                    await (addToCartButtons[0]).click(); 
+                    //await browser.pause(500);
+                }
+                await this.viewCart.click();
+                await console.log('checking if 1 item is added');
+                const crtAddBtn = (await this.removeFromCarty()).length
+                if(crtAddBtn != 1) {
+                    await console.log(crtAddBtn + 'this should be 1');
+                    await this.verifyAfterBad.waitForExist({ timeout: 250 });
+                }
+            }
+            else {
+                await this.verifyAfterBad.waitForExist({ timeout: 250 });
+            }
+        }
+        else {
+            await this.booleanNeeded.waitForExist({ timeout: 250 });
+        }
+
+    }
+
+/* // NOTE! this is another laternate version of the
+// runLength = false test
+else if (runLength == false) {
+            await console.log('running longer test');
             if(addToCartButtons.length > 0) {
                 for(let i = 0; i < 5; i++) {
     
@@ -76,10 +122,11 @@ class ItemAdd extends OpenPage{
         else {
             await this.booleanNeeded.waitForExist({ timeout: 250 });
         }
+*/
 
-    }
 
-    /* //   NOTE! for some reason this doesnt work when the one above exists.
+    /* //   NOTE! this is an alt version that's kinda weird
+    // for some reason this doesnt work when the one above exists.
     async negAddAllToBag2() {
         const initAddBtn = (await this.addToCarty()).length;
         let addToCartButtons = (await this.addToCarty());
--- test/specs/addToCart.spec.js
+++ test/specs/addToCart.spec.js
@@ -3,12 +3,12 @@
 import ItemAdd from'../pageobjects/selectItemAdd.js';
 import LoginPage from '../pageobjects/login.js';
 describe('adding items to cart tests', () => {
-    it('positive adding item to cart', async () => {
-        await LoginPage.goodUserPass(false, 'standard_user','secret_sauce');
-        await ItemAdd.AddAllToBag(true);
-    });
     it('negative adding item to cart', async () => {
         await LoginPage.goodUserPass(false, 'standard_user','secret_sauce');
         await ItemAdd.AddAllToBag(false);
+    });
+    it('positive adding item to cart', async () => {
+        await LoginPage.goodUserPass(false, 'standard_user','secret_sauce');
+        await ItemAdd.AddAllToBag(true);
     });
 });
